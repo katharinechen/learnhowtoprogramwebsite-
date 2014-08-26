e97785801a84745bb6ee948fa5618d0594238c97
Katharine Chen & Diane Douglas <student@epicodus.com>
2014-08-26 09:43:42 -0700

    Add validation in Lesson class

---
+++ app/models/lesson.rb
@@ -0,0 +1,6 @@
+class Lesson < ActiveRecord::Base
+
+  validates :name, :presence => true
+  validates :content, :presence => true
+
+end
--- spec/lesson_spec.rb
+++ spec/lesson_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+describe "Lesson" do
+
+  it "validates the presence of name" do
+    new_lesson = Lesson.new({name: "Learn Ruby", content: "blah blah blah"})
+    expect(new_lesson.save).to eq true
+
+    new_lesson1 = Lesson.new({content: "Blah blah"})
+    expect(new_lesson1.save).to eq false
+  end
+
+  it "validates the presence of content" do
+    new_lesson1 = Lesson.new({name: "Learn Ruby"})
+    expect(new_lesson1.save).to eq false
+  end
+
+end
