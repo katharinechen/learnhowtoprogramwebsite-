1efaca8d980ea201e49ad997a2188ec3c8375f7b
Katharine Chen & Diane Douglas <student@epicodus.com>
2014-08-26 10:27:09 -0700

    add new and create to Lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,4 +5,18 @@ class LessonsController < ApplicationController
     render('lesson/index.html.erb')
   end
 
+  def new
+    render('lesson/new.html.erb')
+  end
+
+  def create
+    @lesson = Lesson.new(params[:lesson])
+    if @lesson.save
+      flash[:notice] = "You created the lesson #{@lesson.name}!"
+      redirect_to('/lessons')
+    else
+      flash[:notice] = "Invalid Entry! Try again!!!!"
+      render('lesson/new.html.erb')
+    end
+  end
 end
